c15ef308848353095b52d88d29232a0b87674d0a
keidis <60551573+keidischoi@users.noreply.github.com>
2026-09-16 04:06:17 +0900

    feat: routes and public request/bid pages

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Custom\MakerBid\Http\Controllers\Admin\CompanyAdminController;
+use Modules\Custom\MakerBid\Http\Controllers\Admin\JobAdminController;
 use Modules\Custom\MakerBid\Http\Controllers\JobController;
 
 Route::get('jobs', [JobController::class, 'index'])->name('jobs.index');
@@ -10,5 +12,12 @@ Route::post('jobs/{id}/bids', [JobController::class, 'bid'])->name('jobs.bid');
 Route::post('jobs/{id}/award', [JobController::class, 'award'])->name('jobs.award');
 
 Route::prefix('admin')->group(function () {
-    Route::get('jobs', [JobController::class, 'index'])->name('admin.jobs.index');
+    Route::get('jobs', [JobAdminController::class, 'index'])->name('admin.jobs.index');
+    Route::patch('jobs/{id}', [JobAdminController::class, 'update'])->name('admin.jobs.update');
+    Route::post('jobs/{id}/hold', [JobAdminController::class, 'hold'])->name('admin.jobs.hold');
+    Route::delete('jobs/{id}', [JobAdminController::class, 'destroy'])->name('admin.jobs.destroy');
+    Route::get('companies', [CompanyAdminController::class, 'index'])->name('admin.companies.index');
+    Route::post('companies', [CompanyAdminController::class, 'store'])->name('admin.companies.store');
+    Route::post('companies/{id}/approve', [CompanyAdminController::class, 'approve'])->name('admin.companies.approve');
+    Route::delete('companies/{id}', [CompanyAdminController::class, 'destroy'])->name('admin.companies.destroy');
 });
