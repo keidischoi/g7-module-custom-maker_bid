abfc32cb0be7d203de9ddda0f90995703432b372
keidis <60551573+keidischoi@users.noreply.github.com>
2026-09-17 00:45:11 +0900

    feat: POST admin jobs approve route

--- src/routes/api.php
+++ src/routes/api.php
@@ -50,13 +50,14 @@ Route::prefix('admin')->middleware(['auth:sanctum', 'throttle:600,1'])->group(fu
     Route::get('jobs', [JobAdminController::class, 'index'])->middleware('permission:admin,custom-maker_bids.jobs.read')->name('admin.jobs.index');
     Route::get('jobs/{id}', [JobAdminController::class, 'show'])->whereNumber('id')->middleware('permission:admin,custom-maker_bids.jobs.read')->name('admin.jobs.show');
     Route::patch('jobs/{id}', [JobAdminController::class, 'update'])->whereNumber('id')->middleware('permission:admin,custom-maker_bids.jobs.update')->name('admin.jobs.update');
+    Route::post('jobs/{id}/approve', [JobAdminController::class, 'approve'])->whereNumber('id')->middleware('permission:admin,custom-maker_bids.jobs.update')->name('admin.jobs.approve');
     Route::post('jobs/{id}/hold', [JobAdminController::class, 'hold'])->whereNumber('id')->middleware('permission:admin,custom-maker_bids.jobs.update')->name('admin.jobs.hold');
     Route::post('jobs/{id}/cancel', [JobAdminController::class, 'cancel'])->whereNumber('id')->middleware('permission:admin,custom-maker_bids.jobs.update')->name('admin.jobs.cancel');
     Route::delete('jobs/{id}', [JobAdminController::class, 'destroy'])->whereNumber('id')->middleware('permission:admin,custom-maker_bids.jobs.delete')->name('admin.jobs.destroy');
     Route::get('job-types', [JobTypeAdminController::class, 'index'])->middleware('permission:admin,custom-maker_bids.jobs.read')->name('admin.job-types.index');
     Route::post('job-types', [JobTypeAdminController::class, 'store'])->middleware('permission:admin,custom-maker_bids.jobs.update')->name('admin.job-types.store');
     Route::patch('job-types/{id}', [JobTypeAdminController::class, 'update'])->whereNumber('id')->middleware('permission:admin,custom-maker_bids.jobs.update')->name('admin.job-types.update');
-    Route::post('job-types/{id}/move', [JobTypeAdminController::class, 'move'])->whereNumber('id')->middleware('permission:admin,custom-maker_bids.jobs.update')->name('admin.job-types.move');
+    Route::post('job-types/{id}/move', [JobTypeAdminController::class, 'move')->whereNumber('id')->middleware('permission:admin,custom-maker_bids.jobs.update')->name('admin.job-types.move');
     Route::delete('job-types/{id}', [JobTypeAdminController::class, 'destroy'])->whereNumber('id')->middleware('permission:admin,custom-maker_bids.jobs.update')->name('admin.job-types.destroy');
     Route::get('bids', [BidAdminController::class, 'index'])->middleware('permission:admin,custom-maker_bids.bids.read')->name('admin.bids.index');
     Route::get('bids/{id}', [BidAdminController::class, 'show'])->whereNumber('id')->middleware('permission:admin,custom-maker_bids.bids.read')->name('admin.bids.show');
