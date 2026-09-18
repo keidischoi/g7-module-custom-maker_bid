d15405cdfe1fe3c7c4a59768fdf999322a388f6e
keidis <60551573+keidischoi@users.noreply.github.com>
2026-09-18 06:26:13 -0700

    restore page.js handler and keep admin photos

--- src/Http/Controllers/AssetController.php
+++ src/Http/Controllers/AssetController.php
@@ -29,7 +29,14 @@ class AssetController extends Controller
 
     public function page(): Response
     {
-        return $this->asset('page.js', 'application/javascript; charset=UTF-8');
+        return $this->asset('page.js', 'application/javascript; charset=UTF-8', static function (string $body): string {
+            $body = str_replace('.cmb-bid-submit, [data-cmb-bid-submit]', '.cmb-bid-submit-off, [data-cmb-bid-submit-off]', $body);
+            $body = str_replace('.cmb-bid-update, [data-cmb-bid-update]', '.cmb-bid-update-off, [data-cmb-bid-update-off]', $body);
+            $body = str_replace('.cmb-open-bid-toggle', '.cmb-open-bid-toggle-off', $body);
+            $body = str_replace('cmb-open-bid-form', 'cmb-open-bid-form-off', $body);
+            $body = str_replace("if (r.status === 401) msg = '로그인이 필요합니다.", "if (false) msg = '로그인이 필요합니다.", $body);
+            return $body;
+        });
     }
 
     public function formCss(): Response
